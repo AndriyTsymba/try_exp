98156b499d23a1464584ab2c46c7ff2f9c9ea9da
Kommandor <a.tsymbaliuk2007@gmail.com>
2023-05-16 19:45:45 +0300

    Exception

--- теорія.py
+++ теорія.py
@@ -9,8 +9,18 @@ print("any code..")
 #2
 def cheker(val_1):
     if type(val_1) != str:
-        raise TypeError(f"sorry,we can't work with{type(val_1)}, на тркба str")
+        raise TypeError(f"sorry,we can't work with{type(val_1)}, на треба str")
     else:
         return val_1
 a = "1234"
 cheker(a)
+#3
+class BuildingHouseErorr(Exception):
+    def __str__(self):
+        return "Щось не те  багото матеріалу"
+def cheker_material_built(amount,limit):
+    if amount > limit:
+        return "Достатньо матеріалів"
+    else:
+       raise BuildingHouseErorr()
+print(cheker_material_built(123,567))
