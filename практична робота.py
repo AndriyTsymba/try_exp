133e5dbd5aedb3b4c9c2dcc9751ad9e3815acc77
Kommandor <a.tsymbaliuk2007@gmail.com>
2023-05-16 20:47:46 +0300

    3 завдання з альбіною

--- практична робота.py
+++ практична робота.py
@@ -1,27 +1,21 @@
 # практична
-#2
-class InvalidPassworld(Exception):
-    def __int__(self, passworld):
-        self.passwold = passworld
-def valide_passworld(passworld):
-    if len(passworld) < 8:
-        raise InvalidPassworld(passworld)
+#3
+class InvalidFileFormatError (Exception):
+    def __init__(self, f):
+        self.f = f
+#FilleNotFoundError
+#UnsupportedFormatError - коли немає розширення .txt
+
+def read_file(f):
+    try:
+        with open(f, "r") as file:
+            content = file.read()
+            print("Вміст файлу:", content)
+    except IOError:
+        raise InvalidFileFormatError(f)
+
 try:
-    passworld = input("Ведіть пароль")
-    valide_passworld(passworld)
-    print("пароль ведененно")
-except InvalidPassworld as e:
-    print(f"Неправильний пароль {e.passwold}, мінімум 8 значеннь")
-class InvalidPasswordError(Exception):
- def __init__(self, password):
-    self.password = password
-def validate_password(password):
-    german_chars = ['ä', 'ö', 'ü', 'ß']
-    if german_chars in password:
-            raise InvalidPasswordError(password)
-try:
-    password = input('Введіть пароль: ')
-    validate_password(password)
-    print('Пароль веденено правильно')
-except InvalidPasswordError as e:
-        print(f'Неправильний пароль: {e.password}')
+    read_file(input("Введіть назву файлу: "))
+except InvalidFileFormatError as e:
+    print(f'Невірний формат файлу {e.f}'
+          f'підтримуються тільки текстові файли')
