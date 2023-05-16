7be6d6985a2e4c3f071969fbf915dd3db5547f72
Kommandor <a.tsymbaliuk2007@gmail.com>
2023-05-16 19:15:00 +0300

    raise знайомстіо

--- теорія.py
+++ теорія.py
@@ -1,1 +1,16 @@
 # теорія
+try:
+    print("start coded")
+    print(error)
+    print("end code")
+except:
+    print("no prodlems")
+print("any code..")
+#2
+def cheker(val_1):
+    if type(val_1) != str:
+        raise TypeError(f"sorry,we can't work with{type(val_1)}, на тркба str")
+    else:
+        return val_1
+a = "1234"
+cheker(a)
